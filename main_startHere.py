abd0fd81ca29ba4c6fd1e9ce6274f585be276bf7
SzilardOvari <N00191534@iadt.ie>
2022-04-25 23:51:37 +0100

    Final code with comments

--- main_startHere.py
+++ main_startHere.py
@@ -1,40 +1,53 @@
 import cv2
 import numpy as np
+#importing the opencv libaries 
 
 def nothing(x):
     pass
 
 def initializeTrackbars(intialTracbacVal=0):
+    #a function to create trackbars
     cv2.namedWindow("Trackbars")
+    #names and creates a window called trackbars
     cv2.resizeWindow("Trackbars",360, 240)
+    #dimensions of the window
     cv2.createTrackbar("Threshold1","Trackbars",intialTracbacVal,255,nothing)
     cv2.createTrackbar("Threshold2","Trackbars", intialTracbacVal,255,nothing)
+    #creates two different slides with values from 0 to 255
+
+#############################################################################################
 
 def valTrackbars():
     Threshold1 = cv2.getTrackbarPos("Threshold1","Trackbars")
     Threshold2 = cv2.getTrackbarPos("Threshold2","Trackbars")
     src= Threshold1,Threshold2
     return src
+    #conncts the trackbars to the thresholdvalues
+    #returns changed values to the source
+
+#################################################################################################
 
 def biggestContour(contours):
     biggest = np.array([])
     max_area = 0
-    #loop through countours list
+    #creates a loop to make sure that only a specific area is highlited 
     for i in contours:
         area = cv2.contourArea(i)
-        #discard areas below treshold
-        if area > 2000:
-            #calculates a contour perimeter -> float
+        #only uses areas below the treshold 
+        if area > 3000:
+            #calculates the contours edge 
             peri = cv2.arcLength(i, True)
-            #calcualtes a curve or a polygon with another curve/polygon with less vertices
+            #calcualtes the curve of the shape
             approx = cv2.approxPolyDP(i, 0.02 * peri, True)
-            # print(f'Area: {area}, Peri: {peri}, Approx: {approx}')
-            # is it a rectangle?
+            # makes sure that it is a rectangle shape othervise it loops back to the start
             if area > max_area and len(approx) == 4:
                 biggest = approx
-                #overwrite max_area for regions that are lareger
+                #overwrite max_area 
                 max_area = area
     return biggest,max_area
+    #returning the new datas 
+
+####################################################################################
 
 def reorder(myPoints):
     try:
@@ -47,18 +60,19 @@ def reorder(myPoints):
         diff = np.diff(myPoints, axis=1)
         myPointsNew[1] =myPoints[np.argmin(diff)]
         myPointsNew[2] = myPoints[np.argmax(diff)]
-    
+    #creates points on the image to alow warping on a later stage (reference points)
         return myPointsNew  
     except ValueError:
         pass
- 
+ ###############################################################################
+
 def drawRectangle(img,biggest,thickness):
     try:
         cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 155, 0), thickness)
         cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 155, 0), thickness)
         cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 155, 0), thickness)
         cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 155, 0), thickness)
-    
+    #draws a rectangle around the given shape using contour lines 
         return img
     except TypeError:
         pass
@@ -98,8 +112,11 @@ while True:
     imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
     imgContours =img.copy()
 
+####################################################################################################
+
     contours,hierarchy =cv2.findContours(imgThreshold,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(imgContours,contours,-125,(0,255,0),10) 
+    #finds the contours on the image or video
 
     biggest,area = biggestContour(contours)
     newPoints = reorder(biggest)
@@ -115,10 +132,12 @@ while True:
     except cv2.error:
         pass
 
-    try:
+    try:#bypasses the error message ,allows the code to run
         imgWarp =cv2.warpPerspective(imgContours,matrix,(widthImg,heightImg))
-    except NameError:
-        pass
+    except NameError:#shows what error to avoid
+        pass#ignores the errror
+
+#####################################################################################
 
     #cv2.imshow("1. Original", img)
     #cv2.imshow("2. Grayscale", imgGray)
